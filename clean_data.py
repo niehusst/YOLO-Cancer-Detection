3501a310ad1f2df6168dafa00aee0d62d1b25c9f
niehusst <liamniehusstaab@gmail.com>
2019-04-12 20:34:43 -0500

    cleaned unused data out of raw csv

---
+++ clean_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+# A quick script to clean unnecessary columns out of the raw 
+# CCC dataset from Kaggle
+
+# read in raw data_label file
+CCC = pd.read_csv('CrowdsCureCancer2017Annotations.csv')
+
+# create a column that is the image path for each row
+CCC["imgPath"] = CCC['patientID'].map(str) + '/' + \
+                CCC['seriesUID'].map(str) + '/' +  \
+                CCC['sliceIndex'].map(str) + '/'
+
+# chop out unnecessary columns
+columns_to_drop = ['order', 'anatomy', 'patientID', 'seriesUID', 'sliceIndex', 'StudyTime', 'SOPClassUID', 'instanceUID', 'length','annotator', 'radiologist_status', 'date_unix', 'date', 'StudyInstanceUID', 'StudyDate', 'StudyTime', '_id']
+
+CCC.drop(columns_to_drop, inplace=True, axis=1)
+
+# write csv with clean data
+CCC.to_csv('CCC_clean.csv')
+
